b7c93e79dbae47d908add6b5e1ac8b0132679577
Raul Santana <ti.raul77@gmail.com>
2024-12-06 17:39:24 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./_components/ui/button"
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <section className="ml-20 mt-20">
+      <h1>Hello</h1>
+      <Button>teste</Button>
+    </section>
+  )
 }
