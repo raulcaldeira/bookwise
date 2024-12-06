b552362b4be8fb1bc31477c67dbcb64802abf1a9
Raul Santana <ti.raul77@gmail.com>
2024-12-06 17:52:04 -0300

    chore: initialize prisma client with caching for development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from "next"
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Bookwise",
   description: "O maior aplicativo de avaliação de livros.",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className="dark">
       <body className="antialiased">{children}</body>
     </html>
-  );
+  )
 }
