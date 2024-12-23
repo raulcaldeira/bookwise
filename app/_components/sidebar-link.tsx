3a398f5946b33a2d122d1e87a531adba602d7b33
Raul Santana <ti.raul77@gmail.com>
2024-12-23 17:37:32 -0300

    fix: ensure sidebar link is highlighted when it matches the current URL

--- app/_components/sidebar-link.tsx
+++ app/_components/sidebar-link.tsx
@@ -14,14 +14,16 @@ interface SidebarLinkProps {
 const SidebarLink = ({ text, url, icon, hide = false }: SidebarLinkProps) => {
   const pathname = usePathname()
 
+  const linkIsActive = pathname.split("/")[1] === url.split("/")[1]
+
   return (
     <div className={`relative flex px-5 ${hide && "hidden"}`}>
       <div
-        className={`absolute left-0 top-0 h-full w-1 rounded-b-full rounded-t-full ${pathname !== url ? "bg-transparent" : "bg-vertical-gradient"}`}
+        className={`absolute left-0 top-0 h-full w-1 rounded-b-full rounded-t-full ${linkIsActive ? "bg-vertical-gradient" : "bg-transparent"}`}
       ></div>
       <Link href={url} className="flex items-center gap-3">
         <div
-          className={`flex items-center gap-3 ${pathname !== url ? "opacity-60" : "font-bold"}`}
+          className={`flex items-center gap-3 ${linkIsActive ? "font-bold" : "opacity-60"}`}
         >
           {icon}
           <span className="text-lg capitalize text-gray-100">{text}</span>
